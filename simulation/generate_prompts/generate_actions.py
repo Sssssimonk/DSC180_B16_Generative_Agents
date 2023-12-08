aae09416d8d4df1072adbb5847942079b7e2aa8f
RoXxxYyy <yehe@ucsd.edu>
2023-12-07 19:22:45 -0800

    add some changes

--- simulation/generate_prompts/generate_actions.py
+++ simulation/generate_prompts/generate_actions.py
@@ -1,25 +1,39 @@
 import json
 import os
+from utils.initialize_model import generate
 
-def generate_action_prompts(town_areas, town_people, locations, global_time, plans, memories):
+# Generate actions and store memories
+def generate_action_results(town_areas, description, locations, plans, memories, model, tokenizer):
+    prompt_meta = '''### Instruction:
+{}
+
+### Response:'''
+    action_results = {}
     script_dir = os.path.dirname(__file__)
     file_path = os.path.join(script_dir, '../prompts_template/action.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
         prompt_template = data['prompt_template']
     action_prompts = {}
-    for location, description in town_areas.items():
+    for location, loc_description in town_areas.items():
         people_in_location = [name for name, loc in locations.items() if loc == location]
-
         for name in people_in_location:
-            person_description = town_people[name]["description"]
+            person_description = description[name]
             person_plan = plans[name]
-            people_descriptions = [f"{person}: {town_people[person]['description']}" for person in people_in_location]
+            people_descriptions = [f"{person}: {description[person]}" for person in people_in_location]
             people_descriptions_str = '. '.join(people_descriptions)
-            memory_text = '. '.join(memories[name][-10:])
+#             memory_text = '. '.join(memories[name][-10:])
 
             prompt = prompt_template.format(name, person_description, person_plan, location, 
-            description, str(global_time), ', '.join(people_in_location), people_descriptions_str)
+            description, ', '.join(people_in_location), people_descriptions_str)
 
             action_prompts[name] = prompt
-    return action_prompts
+    for name, prompt in action_prompts.items():
+        action_results[name] = generate(prompt_meta.format(prompt), model, tokenizer)
+        
+    for location in town_areas.keys():
+        people_in_location = [name for name, loc in locations.items() if loc == location]
+        for name in people_in_location:
+            for name_two in people_in_location:
+                memories[name].append('[Person: {}. Memory: {}]\n'.format(name_two, action_results[name_two]))
+    return action_results
--- simulation/generate_prompts/generate_plans.py
+++ simulation/generate_prompts/generate_plans.py
@@ -2,15 +2,18 @@ import os
 import json
 from utils.initialize_model import generate
 
-def generate_plans(town_people, model, tokenizer):
+def generate_plans(description, model, tokenizer, plans):
+    prompt_meta = '''### Instruction:
+{}
+
+### Response:'''
+    
     script_dir = os.path.dirname(__file__)
     file_path = os.path.join(script_dir, '../prompts_template/plan.json')
     with open(file_path, 'r') as file:
         data = json.load(file)
         prompt_template = data['prompt_template']
-    plans = {}
-    for name, info in town_people.items():
-        prompt = prompt_template.format(name, info['description'], , ', '.join(list(town_people.keys())))
-        plans[name] = generate(prompt, model, tokenizer)
-        print(name, plans[name])
-    return plans
+    for name, info in description.items():
+        prompt = prompt_template.format(name, info, ', '.join(list(description.keys())))
+        plans[name] = generate(prompt_meta.format(prompt), model, tokenizer)
+    return None
